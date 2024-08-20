9486d5eb2ff6c5ba2b73b53138daa51a7736da88
Alina Haisiuk <alina.gaysyuk@gmail.com>
2024-08-20 15:09:13 +0300

    fixed version

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
@@ -12,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
